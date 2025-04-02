00fba4c7f451661e2f4416bd51386f0f4ca0ff49
Daspriyabratya007 <your-email@example.com>
2025-04-02 10:50:56 +0530

    components are added

--- NewBlock/storyteller/src/App.js
+++ NewBlock/storyteller/src/App.js
@@ -1,10 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
-
+import Cards from './Components/Cards'
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+      <Cards/>
+      <Cards/>
+      <Cards/>
+      <Cards/>
+      <Cards/>
+      <Cards/>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
---
+++ NewBlock/storyteller/src/Components/Cards.js
@@ -0,0 +1,9 @@
+function Cards() {
+    return (
+        <div className="cards-layout">
+            <p>Priyabrata</p>
+            <img src="./image.png"/>
+        </div>
+    );
+}
+export default Cards;
