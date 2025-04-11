052cdda0e419eda9cd70e966bbc7861814406a0c
Daspriyabratya007 <your-email@example.com>
2025-04-12 01:13:51 +0530

    cards eddited and solved the problems given by Dada

--- NewBlock/storyteller/src/App.js
+++ NewBlock/storyteller/src/App.js
@@ -1,28 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
 import Cards from './Components/Cards';
-import mockSampleData from "./MockData/DummyData.json"
+import DummyData from "./MockData/DummyData.json"
 function App() {
-  console.log(mockSampleData);
-  console.log(mockSampleData.users);
+  console.log(DummyData);
+  console.log(DummyData.users);
   return (
+   
     <div>
+       <span class ="My Line">
       <h1> This is my First ever React Application</h1>
       {
         // Calling the Cards component
       }
+      </span>
+      <hr></hr>
+      
       <div className="cards">
       
       {
-        mockSampleData.users.map((item)=>{
+        DummyData.users!=0?
+        DummyData.users.map((item)=>{
           console.log("Hello I'm from MAP: "+item.firstName+" "+item.lastName);
-          return <Cards name={`${item.firstName}${item.lastName}`} 
-          imgPath={item.image} username={item.username}
-          eyecolor={item.eyeColor}
-          age={item.age}
-          BloodG={item.bloodGroup}
-          />
-        })
+          return <Cards data = {item}/>
+        }):<h1 class="NoData">Mo user Details found at this Moment</h1>
       }
       </div>
       
--- NewBlock/storyteller/src/Components/Cards.js
+++ NewBlock/storyteller/src/Components/Cards.js
@@ -1,43 +1,15 @@
-function Cards({name,imgPath,username,eyecolor,age,address,BloodG}) {
-    //name and imgPath is called propsr
-    // let bgColour = "";
-    // if(eyecolor=="blue")
-    // {
-    //     bgColour = "rgb(2, 2, 36)";
-    // }
-    // else if (eyecolor=="Red")
-    // {
-    //     bgColour = "rgb(244, 3, 3)";
-    // }
-    // else if (eyecolor == "Hazel")
-    // {
-    //     bgColour ="rgb(165, 42, 42)";
-    // }
-    // else if (eyecolor == "Amber")
-    // {
-    //     bgColour ="rgb(255, 191, 0)";
-    // }
-    // else if (eyecolor=="Green")
-    // {
-    //     bgColour =  "rgb(63, 244, 3)";
-    // }
-    // else if (eyecolor=="Hazel")
-    // {
-    //     bgColour = "rgb(165, 42, 42)";
-    // }
-    // else{
-    //     bgColour = "rgb(3, 3, 3)";
-    // }
-
+function Cards({data}) {
     return (
 
-        <div className="cards-layout">
-            <p>{name}</p>
-            <img src={imgPath} alt ="Image is broken"/>
-            <p>{username}</p>
-            <p>{eyecolor}</p>
-            <p>{age}</p>
-            <p>{BloodG}</p>
+        <div className="cards-layout" style={{backgroundColor:data.eyeColor}}>
+            <p>{`${data.firstName} ${data.lastName}`}</p>
+            <img src={data.image} alt ="Image is broken"/>
+            <p>Username:@{data.username}</p>
+            <p>My EyeColour is {data.eyeColor}</p>
+            <p>My age is:{data.age}</p>
+            <p>My Blood groop is:{data.bloodGroup}</p>
+            <p>My hair colour is :{data.hair.color}</p>
+            <p>My hair Type is :{data.hair.type}</p>
 
         </div>
     );
